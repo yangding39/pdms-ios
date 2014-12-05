42126f966ee18c7ef4fe9157be8293d41f341d89
yangding39 <yangding39@gmail.com>
2014-12-05 10:32:04 +0800

    inital project

---
+++ pdms-ios/SwiftObjectcBridge.h
@@ -0,0 +1,13 @@
+//
+//  SwiftObjectcBridge.h
+//  pdms-dynamic
+//
+//  Created by IMEDS on 14-11-28.
+//  Copyright (c) 2014年 IMEDS. All rights reserved.
+//
+
+#ifndef pdms_dynamic_SwiftObjectcBridge_h
+#define pdms_dynamic_SwiftObjectcBridge_h
+#import <AFNetworking/AFNetworking.h>
+
+#endif
